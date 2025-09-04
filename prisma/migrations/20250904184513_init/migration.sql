b9ff7560917f4c814a9be9e6bbe4a3678585a914
Karan <kb466444@gmail.com>
2025-09-05 00:16:04 +0530

    fix: error fixed

--- prisma/migrations/20250904184513_init/migration.sql
+++ prisma/migrations/20250904184513_init/migration.sql
@@ -12,7 +12,6 @@ CREATE TABLE "public"."User" (
 CREATE TABLE "public"."Tour" (
     "id" TEXT NOT NULL,
     "title" TEXT NOT NULL,
-    "steps" JSONB NOT NULL,
     "isPublic" BOOLEAN NOT NULL DEFAULT false,
     "userId" TEXT NOT NULL,
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
@@ -20,8 +19,27 @@ CREATE TABLE "public"."Tour" (
     CONSTRAINT "Tour_pkey" PRIMARY KEY ("id")
 );
 
+-- CreateTable
+CREATE TABLE "public"."TourStep" (
+    "id" TEXT NOT NULL,
+    "title" TEXT NOT NULL,
+    "description" TEXT NOT NULL,
+    "mediaUrl" TEXT,
+    "order" INTEGER NOT NULL,
+    "tags" TEXT[],
+    "duration" INTEGER,
+    "interactive" JSONB,
+    "tourId" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "TourStep_pkey" PRIMARY KEY ("id")
+);
+
 -- CreateIndex
 CREATE UNIQUE INDEX "User_email_key" ON "public"."User"("email");
 
 -- AddForeignKey
 ALTER TABLE "public"."Tour" ADD CONSTRAINT "Tour_userId_fkey" FOREIGN KEY ("userId") REFERENCES "public"."User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "public"."TourStep" ADD CONSTRAINT "TourStep_tourId_fkey" FOREIGN KEY ("tourId") REFERENCES "public"."Tour"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
