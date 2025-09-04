fc862776392bfffb4f0a0b6196335137e835e059
Karan <kb466444@gmail.com>
2025-09-04 18:16:21 +0530

    fix: api

--- app/api/tours/get-tours/[id]/[tourId]/route.ts
+++ app/api/tours/get-tours/[id]/[tourId]/route.ts
@@ -3,7 +3,7 @@ import { NextRequest, NextResponse } from "next/server";
 
 export async function GET(
   req: NextRequest,
-  context: { params: { id: string; tourId: string } }
+  context: { params: Promise<{ id: string; tourId: string }> }
 ) {
   try {
     const { id: userId, tourId } = context.params;
